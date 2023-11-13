72521adf6f36659fd1d5eeda2bb73c00cbff2e1f
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-11-13 20:39:50 +0300

    Add files via upload

---
+++ SQL_PRACTICE.sql
@@ -0,0 +1,33 @@
+--CREATE TABLE TeamDemographics
+--(
+--StudentID int,
+--FirstName varchar(50),
+--LastName varchar(50),
+--Department varchar(50),
+--)
+
+--Create Table StudentSalary 
+--(StudentID int, 
+--JobTitle varchar(50), 
+--Salary int
+--)
+
+--INSERT INTO TeamDemographics VALUES
+--(1001, 'Jeremy', 'Lin', 'CSE'),
+--(1002, 'Nelson', 'Samuel', 'CSE'),
+--(1003, 'Steph', 'Curry', 'Architecture'),
+--(1004, 'Russell', 'Westbrook', 'CSE'),
+--(1005, 'Isaiah', 'Thomas', 'Computer Science'),
+--(1006, 'Kevin', 'Durant', 'CSE'),
+--(1007, 'Chris', 'Paul', 'CSE')
+
+INSERT INTO StudentSalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
