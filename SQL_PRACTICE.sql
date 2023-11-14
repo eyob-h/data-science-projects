b27282c366f8572c9278b3136d537372dda9df19
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-11-14 18:14:11 +0300

    Update SQL_PRACTICE.sql

--- SQL_PRACTICE.sql
+++ SQL_PRACTICE.sql
@@ -21,13 +21,25 @@
 --(1006, 'Kevin', 'Durant', 'CSE'),
 --(1007, 'Chris', 'Paul', 'CSE')
 
-INSERT INTO StudentSalary VALUES
-(1001, 'Salesman', 45000),
-(1002, 'Receptionist', 36000),
-(1003, 'Salesman', 63000),
-(1004, 'Accountant', 47000),
-(1005, 'HR', 50000),
-(1006, 'Regional Manager', 65000),
-(1007, 'Supplier Relations', 41000),
-(1008, 'Salesman', 48000),
-(1009, 'Accountant', 42000)
+--INSERT INTO StudentSalary VALUES
+--(1001, 'PF', 450000),
+--(1002, 'PG', 360000),
+--(1003, 'C', 630000),
+--(1004, 'SG', 470000),
+--(1005, 'PF', 500000),
+--(1006, 'SG', 650000),
+--(1007, 'PF', 410000),
+--(1008, 'SF', 480000),
+--(1009, 'PG', 420000)
+
+--SELECT * FROM TeamDemographics
+--WHERE FirstName = 'Kevin';
+--WHERE FirstName <> 'Steph';
+
+SELECT * FROM TeamDemographics
+--WHERE FirstName LIKE '%n'
+--WHERE FirstName LIKE 'k%i%'
+--WHERE FirstName is NOT NULL
+WHERE FirstName IN ('Russell', 'Chris')
+
+
